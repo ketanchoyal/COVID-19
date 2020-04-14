6d6ba93d24de657971cc069436a0d94e38247ca1
Ketan Choyal <ketanchoyal@gmail.com>
2020-04-13 20:23:58 -0400

    Initial Commit

---
+++ COVID-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by Ketan Choyal on 2020-04-13.
+//  Copyright © 2020 Ketan Choyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
