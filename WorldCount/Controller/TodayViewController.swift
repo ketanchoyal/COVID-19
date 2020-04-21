ce79b9b806a23d7479574770f23463d8e4bd727f
Ketan Choyal <ketanchoyal@gmail.com>
2020-04-21 16:30:40 -0400

    Widget Code Organized

--- WorldCount/Controller/TodayViewController.swift
+++ WorldCount/Controller/TodayViewController.swift
@@ -24,16 +24,10 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
         CovidManager.getCountriesData { (success, coronaData) in
             if success {
                 guard let coronaData = coronaData else {
-                    completionHandler(NCUpdateResult.failed)
+                    completionHandler(NCUpdateResult.noData)
                     return
                 }
                 let worldData = coronaData.response.filter { (data) -> Bool in
@@ -58,7 +52,6 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     
     fileprivate func getCovidData() {
-        // Do any additional setup after loading the view.
         CovidManager.getCountriesData { (success, coronaData) in
             if success {
                 guard let coronaData = coronaData else {
