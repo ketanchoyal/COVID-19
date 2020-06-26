eb18af84f1a6eec25907f923991602776caa8752
ketanchoyal <sharmaketanr@gmail.com>
2020-06-26 12:16:47 -0400

    minor changes

---
+++ Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  COVID-19
+//
+//  Created by Ketan Choyal on 2020-04-14.
+//  Copyright © 2020 Ketan Choyal. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    struct Keys {
+        static let rapid_Covid_Key: String = "e9d8baaac0mshc8b13c495ddc50cp187faejsnacc9cf1cb06a"
+    }
+    
+}
